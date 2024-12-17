1ea32fc48abed2d3eeb2b804fa35e55363660eef
Daniel Carpio <carpiordaniel@gmail.com>
2024-12-16 21:55:50 -0500

    UPDATE LOGIN URL HOST

--- src/globals/constantes.js
+++ src/globals/constantes.js
@@ -2,7 +2,8 @@ export const COLORS = {
   PRIMARY: "#333333",
 };
 
-export const URL_BASE = "http://localhost:8081";
+// export const URL_BASE = "http://localhost:8081";
+export const URL_BASE = "https://wiwuakuna.up.railway.app";
 
 export const ENDPOINTS = {
   GET_FINCA: `${URL_BASE}/api/animales-fincas/fincas`,
