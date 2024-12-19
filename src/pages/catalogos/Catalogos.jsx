e26a7d87f6e22cb3db78163e81d50c1838e44e7e
fmoreiraUnir2024 <franciscosimon.moreira045@comunidadunir.net>
2024-12-18 22:56:58 -0500

    sdf
    sddf

--- src/pages/catalogos/Catalogos.jsx
+++ src/pages/catalogos/Catalogos.jsx
@@ -1,31 +1,43 @@
-import { Route, Routes } from "react-router-dom"
-import routes from "@/routes";
-import {
-  Sidenav,
-  DashboardNavbar,
-  Configurator,
-  Footer,
-} from "@/widgets/layout";
-const Catalogos = () => {
+import React, { useState, useEffect } from 'react';
+import { TextField, MenuItem } from '@mui/material';
+import axiosClient from '../../axios/apiClient';
+import { DICCIONARIOS } from '@/globals/constantes';
+//sjdsabdjas
+const Catalogos = ({ tabla, label, value, onChange }) => {
+  const [catalogoData, setCatalogoData] = useState([]);
+
+  useEffect(() => {
+    if (tabla) {
+      getCatalogoData(tabla);
+    }
+  }, [tabla]);
+
+  const getCatalogoData = async (tabla) => {
+    try {
+      const response = await axiosClient.get(DICCIONARIOS.GET_BY_TABLA(tabla));
+      setCatalogoData(response.data);
+    } catch (error) {
+      console.error('Error al obtener datos del catálogo:', error);
+    }
+  };
+
   return (
-    <>
-    
-    <Routes>
-        {/* {routes.map(
-          ({ layout, pages }) =>
-            layout === "catalogos" &&
-          pages.map(({ path, element }) => (
-            <Route exact path={path} element={element} />
-          ))
-        )} */}
-      </Routes>
-      <div className="text-blue-gray-600">
-          <Footer />
-        </div>
+    <TextField
+      select
+      fullWidth
+      label={label}
+      value={value}
+      onChange={onChange}
+      variant="outlined"
+      margin="normal"
+    >
+      {catalogoData.map((item) => (
+        <MenuItem key={item.id} value={item.codigo}>
+          {item.nombre}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+};
 
-          </>
-
-  )
-}
-
-export default Catalogos
+export default Catalogos;
